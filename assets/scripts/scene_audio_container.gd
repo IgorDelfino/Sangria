d163d7b4704859489cd23650179c7985f72cbeb6
IgorDelfino <igordelfinobarbosa56@gmail.com>
2025-04-17 20:58:16 -0300

    added menu song loop

--- assets/scripts/scene_audio_container.gd
+++ assets/scripts/scene_audio_container.gd
@@ -3,6 +3,7 @@ extends Node
 class_name SceneAudioContainer
 
 @export var ambience : AudioStream
-@export var soundtrack : AudioStream
+@export var soundtrack : Array[AudioStream]
 
+@export var has_song_intro : bool = false
 @export var should_play_music_if_null : bool = false
